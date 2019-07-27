08f3953a2da032c7617e39576bcb5705359e0804
Alexander Böhm <tools@boehm.work>
2019-07-27 19:13:36 +0200

    Split the bash tasks to separate files

---
+++ tasks/release-clear.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+rm -rf .releases/include-fussball-de-widgets/trunk/*
---
+++ tasks/release-copy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mkdir -p .releases/include-fussball-de-widgets/trunk
+cp -r ./app/dist/* .releases/include-fussball-de-widgets/trunk
