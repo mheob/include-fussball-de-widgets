f329b5035852224542969b70d2e18fe602fa5810
TODO <todo-actions[bot]@users.noreply.github.com>
2020-04-14 18:07:17 +0000

    Collect TODO comments

--- src/Frontend/Fubade.php
+++ src/Frontend/Fubade.php
@@ -44,7 +44,7 @@ final class Fubade {
 	 * @return string The output to the sourcecode.
 	 */
 	public function output( array $attr ): string {
-		// TODO: Configure default setting in the admin area.
+		// TODO [$5e95fbd5c099020007a01c47]: Configure default setting in the admin area.
 		$this->setAttr( $attr );
 
 		$this->attr = [
--- src/Utils/Logging/LoggerBase.php
+++ src/Utils/Logging/LoggerBase.php
@@ -72,5 +72,5 @@ abstract class LoggerBase {
 	 */
 	abstract protected function logWidgetInfo( array $arr ): void;
 
-	// TODO: Add a file or database logging system.
+	// TODO [$5e95fbd5c099020007a01c48]: Add a file or database logging system.
 }
